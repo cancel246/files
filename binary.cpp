8b5bd1edc01577db2b53c10c03c746b245e8850f
cancel246 <cancel246@users.noreply.github.com>
2018-02-20 11:53:23 -0400

    Finished Lab 3

---
+++ binary.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+int main( )
+{
+   //create some data
+   short int a = -6730;
+   float b = 68.123; 
+   char c = 'J';
+ 
+	ofstream myfile {"abc.bin", ios::binary};
+	
+	if (myfile)
+	{
+		myfile << a << " " << b << " " << c;
+		myfile.write(reinterpret_cast<char*>(&a), sizeof(a));
+		myfile.write(reinterpret_cast<char*>(&b), sizeof(b));
+		myfile.write(reinterpret_cast<char*>(&c), sizeof(c));
+	}
+	else
+	cout << "error opening abc.bin";
+	myfile.close();
+	
+	   return 0;
+}
